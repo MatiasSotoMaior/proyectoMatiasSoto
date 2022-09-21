6e2e5c8cf578aa9c5d4ab973511c2b93accf7622
MatiasSotoMaior <matysotomaiorfer@gmail.com>
2022-09-20 22:41:35 -0300

    optimizando

---
+++ optimizandoProyecto/stock.js
@@ -0,0 +1,11 @@
+let stockProductos = [
+    { id: 1, nombre: "Patagonia Amber 710cc", tipo: "ceveza", cantidad: "1", desc: "Botella de 750ml", precio: 1000, img: "img/pataAmber.png" },
+    { id: 2, nombre: "Patagonia Weisse 710cc", tipo: "ceveza", cantidad: "1", desc: "Botella de 750ml", precio: 1000, img: "img/pataWeisse.png" },
+    { id: 3, nombre: "Patagonia Bohemian 710cc", tipo: "ceveza", cantidad: "1", desc: "Botella de 750ml", precio: 1000, img: "img/pataBohemian.png" },
+    { id: 4, nombre: "Patagonia Amber 710cc", tipo: "ceveza", cantidad: "1", desc: "Botella de 750ml", precio: 1000, img: "img/pataAmber.png" },
+    { id: 5, nombre: "Patagonia Weisse 710cc", tipo: "ceveza", cantidad: "1", desc: "Botella de 750ml", precio: 1000, img: "img/pataWeisse.png" },
+    { id: 6, nombre: "Patagonia Bohemian 710cc", tipo: "ceveza", cantidad: "1", desc: "Botella de 750ml", precio: 1000, img: "img/pataBohemian.png" },
+    { id: 7, nombre: "Patagonia Amber 710cc", tipo: "ceveza", cantidad: "1", desc: "Botella de 750ml", precio: 1000, img: "img/pataAmber.png" },
+    { id: 8, nombre: "Patagonia Weisse 710cc", tipo: "ceveza", cantidad: "1", desc: "Botella de 750ml", precio: 1000, img: "img/pataWeisse.png" },
+    { id: 9, nombre: "Patagonia Bohemian 710cc", tipo: "ceveza", cantidad: "1", desc: "Botella de 750ml", precio: 1000, img: "img/pataBohemian.png" },
+]
